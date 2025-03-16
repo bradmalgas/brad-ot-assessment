e0526083738104ff48eee60fdefcb05be209b78c
Brad Malgas <bradstermalgas@gmail.com>
2025-03-16 23:39:30 +0200

    Revert API project to boilerplate

--- src/OT.Assessment.App/Program.cs
+++ src/OT.Assessment.App/Program.cs
@@ -1,53 +1,15 @@
 using System.Reflection;
-using Microsoft.EntityFrameworkCore;
-using OT.Assessment.App.Messaging.Implementation;
-using OT.Assessment.App.Services.Implementation;
-using OT.Assessment.App.Services.Interfaces;
-using OT.Assessment.Shared.Data.Implementation;
-using OT.Assessment.Shared.Data.Interfaces;
-using OT.Assessment.Shared.Messaging;
-using OT.Assessment.Shared.Messaging.Implementation;
-using OT.Assessment.Shared.Messaging.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckl
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSwaggerGen(options =>
 {
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
-
-builder.Services.AddDbContext<CasinoWagersDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
-        b => b.MigrationsAssembly("OT.Assessment.Consumer")));
-builder.Services.Configure<RabbitMqConfiguration>(builder.Configuration.GetSection("RabbitMq"));
-builder.Services.AddScoped<ICasinoWagerRepository, CasinoWagerRepository>();
-builder.Services.AddScoped<IPlayersRepository, PlayersRepository>();
-builder.Services.AddScoped<ICasinoWagerApiService, CasinoWagerApiService>();
-builder.Services.AddSingleton<ICasinoWagerPublisher, CasinoWagerPublisher>();
-builder.Services.AddSingleton<IRabbitMqChannelFactory, RabbitMqChannelFactory>();
-builder.Services.AddSingleton<IRabbitMqConnectionManager, RabbitMqConnectionManager>();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAllOrigins",
-    builder =>
-    {
-        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-    }
-    );
-});
-
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.ListenLocalhost(5021, options => options.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http1AndHttp2);
-    serverOptions.ListenLocalhost(7120, options =>
-    {
-        options.UseHttps();
-    });
 });
 
 var app = builder.Build();
@@ -63,8 +25,6 @@ if (app.Environment.IsDevelopment())
         opts.DisplayRequestDuration();
     });
 }
-
-app.UseCors("AllowAllOrigins");
 
 app.UseHttpsRedirection();
 
--- src/OT.Assessment.App/Services/Interfaces/ICasinoWagerApiService.cs
+++
@@ -1,8 +0,0 @@
-namespace OT.Assessment.App.Services.Interfaces;
-
-public interface ICasinoWagerApiService
-{
-    Task PublishAsync(CasinoWagerEventDTO casinoWager);
-    Task<PaginatedResult<CasinoWagerDTO>> GetWagersByPlayerAsync(Guid playerId, int pageSize, int page);
-    Task<List<PlayerTopSpenderDTM>> GetTopSpendersAsync(int count);
-}
