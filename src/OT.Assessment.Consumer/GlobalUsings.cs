acd0ac19e837d3f31ba9bb6132ae66a4c1480ed0
Brad Malgas <bradstermalgas@gmail.com>
2025-03-16 20:48:08 +0200

    updated global usings and DI

--- src/OT.Assessment.Consumer/GlobalUsings.cs
+++ src/OT.Assessment.Consumer/GlobalUsings.cs
@@ -3,3 +3,5 @@
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
+global using OT.Assessment.Shared.Models;
+global using OT.Assessment.Consumer.Services.Interfaces;
--- src/OT.Assessment.Consumer/Program.cs
+++ src/OT.Assessment.Consumer/Program.cs
@@ -1,7 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using OT.Assessment.Consumer.Messaging.Implementation;
+using OT.Assessment.Consumer.Services.Implementation;
+using OT.Assessment.Shared.Consumer.Interfaces;
 using OT.Assessment.Shared.Data.Implementation;
 using OT.Assessment.Shared.Data.Interfaces;
+using OT.Assessment.Shared.Messaging;
+using OT.Assessment.Shared.Messaging.Implementation;
+using OT.Assessment.Shared.Messaging.Interfaces;
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration(config =>
@@ -16,14 +22,23 @@ var host = Host.CreateDefaultBuilder(args)
 
         services.AddDbContext<CasinoWagersDbContext>(options => options.UseSqlServer(context.Configuration.GetConnectionString("DefaultConnection"),
         b => b.MigrationsAssembly("OT.Assessment.Consumer")));
+        services.Configure<RabbitMqConfiguration>(context.Configuration.GetSection("RabbitMq"));
         services.AddScoped<ICasinoWagerRepository, CasinoWagerRepository>();
         services.AddScoped<IPlayersRepository, PlayersRepository>();
-
+        services.AddSingleton<IRabbitMqConnectionManager, RabbitMqConnectionManager>();
+        services.AddSingleton<IRabbitMqChannelFactory, RabbitMqChannelFactory>();
+        services.AddScoped<ICasinoWagerService, CasinoWagerService>();
+        services.AddSingleton<ICasinoWagerConsumer, CasinoWagerConsumer>();
+        services.AddScoped<ICasinoWagersDbContext, CasinoWagersDbContext>();
+        services.AddSingleton<ICasinoWagerServiceFactory, CasinoWagerServiceFactory>();
     })
     .Build();
 
 var logger = host.Services.GetRequiredService<ILogger<Program>>();
 logger.LogInformation("Application started {time:yyyy-MM-dd HH:mm:ss}", DateTime.Now);
+
+var consumer = host.Services.GetService<ICasinoWagerConsumer>();
+await consumer.ConsumeAsync();
 
 await host.RunAsync();
 
