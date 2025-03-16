68c1ad506ff2f2c84da5e39f4675e39df89ae675
Brad Malgas <bradstermalgas@gmail.com>
2025-03-16 20:48:31 +0200

    Added versioning to casinowager

--- src/OT.Assessment.Shared/Entities/CasinoWagerEntity.cs
+++ src/OT.Assessment.Shared/Entities/CasinoWagerEntity.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OT.Assessment.Shared.Models;
 
 public class CasinoWagerEntity
@@ -9,6 +11,8 @@ public class CasinoWagerEntity
     public decimal Amount { get; set; }
     public DateTime WagerDateTime { get; set; }
     public DateTime CreatedAt { get; set; }
+    [Timestamp]
+    public byte[] Version { get; set; }
 
     public virtual PlayerEntity Player { get; set; }
 }
