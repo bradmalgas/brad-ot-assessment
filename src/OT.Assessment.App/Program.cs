39a638722527f9e8cdf2e7964b82577d013ad46a
Brad Malgas <bradstermalgas@gmail.com>
2025-03-17 09:27:36 +0200

    Updated publish endpoint

--- src/OT.Assessment.App/Program.cs
+++ src/OT.Assessment.App/Program.cs
@@ -1,5 +1,9 @@
 using System.Reflection;
+using System.Text;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
+using OT.Assessment.Shared.Models;
+using RabbitMQ.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -26,11 +30,42 @@ if (app.Environment.IsDevelopment())
     });
 }
 
+var hostName = builder.Configuration["RabbitMq:HostName"];
+var userName = builder.Configuration["RabbitMq:UserName"];
+var password = builder.Configuration["RabbitMq:Password"];
+var queueName = builder.Configuration["RabbitMq:QueueName"];
+var exchangeName = builder.Configuration["RabbitMq:ExchangeName"];
+
 app.MapGet("/", () => "The API is running")
 .WithName("Health Check");
 
 //POST api/player/casinowager
-app.MapPost("api/player/casinowager", () => "Post wager works");
+app.MapPost("api/player/casinowager", async ([FromBody] CasinoWagerRequest request) =>
+{
+    var dto = new CasinoWagerEventDTM
+    {
+        WagerId = request.WagerId,
+        GameName = request.GameName,
+        Provider = request.Provider,
+        Amount = request.Amount,
+        CreatedDateTime = request.CreatedDateTime,
+        AccountId = request.AccountId,
+        Username = request.Username
+    };
+    var factory = new ConnectionFactory
+    {
+        HostName = hostName,
+        UserName = userName,
+        Password = password,
+    };
+    using var connection = await factory.CreateConnectionAsync();
+    using var channel = await connection.CreateChannelAsync();
+    await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+    var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(dto));
+    await channel.BasicPublishAsync(exchange: string.Empty, routingKey: queueName, body: body);
+})
+.WithName("PublishCasinoWager")
+.WithOpenApi();
 
 //GET api/player/{playerId}/wagers
 app.MapGet("api/player/{playerId}/wagers", ([FromRoute] Guid playerId) => "Get player wagers works");
